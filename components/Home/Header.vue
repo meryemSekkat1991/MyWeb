6a5600458ab4b081605cfc090ed22196ed34d85e
meryem sekkat <34552218+meryemSekkat1991@users.noreply.github.com>
2021-10-11 17:52:38 +0100

    feat pk2 (#2)

--- components/Home/Header.vue
+++ components/Home/Header.vue
@@ -1,161 +1,171 @@
-<template>
-  <header class="header-one">
-    <!-- Start top bar -->
-    <div class="topbar-area fix hidden-xs">
-      <div class="container">
-        <div class="row">
-          <div class=" col-md-9 col-sm-9">
-            <div class="topbar-left">
-              <ul>
-                <li><a href="#"><i class="fa fa-envelope"></i> info@MyWeb3.com</a></li>
-                <li><a href="#"><i class="fa fa-phone-square"></i> +3234-7665545</a></li>
-                <li><a href="#"><i class="fa fa-clock-o"></i> Mon - Fri: 10:00 - 18:00</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md-3 col-sm-3">
-            <div class="top-social">
-              <ul>
-                <li><a href="#"><i class="fa fa-skype"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-google"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- End top bar -->
-    <!-- header-area start -->
-    <div id="sticker" class="header-area hidden-xs">
-      <div class="container">
-        <div class="row">
-          <!-- logo start -->
-          <div class="col-md-3 col-sm-3">
-            <div class="logo">
-              <!-- Brand -->
-              <nuxt-link class="navbar-brand " to="/">
-                <img src="/img/logo/logo2.png" alt="">
-              </nuxt-link>
-            </div>
-            <!-- logo end -->
-          </div>
-          <div class="col-md-9 col-sm-9">
-            <div class="header-right-link">
-              <!-- search option end -->
-              <nuxt-link class="s-menu" to="login">Get in Estimate</nuxt-link>
-            </div>
-            <!-- mainmenu start -->
-            <nav class="navbar navbar-default">
-              <div class="collapse navbar-collapse" id="navbar-example">
-                <div class="main-menu">
-                  <ul class="nav navbar-nav navbar-right">
-                    <li><nuxt-link class="pagess" to="#">Home</nuxt-link>
-                      <ul class="sub-menu">
-                        <li><nuxt-link to="/">Home 01</nuxt-link></li>
-                        <li><nuxt-link to="/index-2">Home 02</nuxt-link></li>
-                        <li><nuxt-link to="/index-3">Home 03</nuxt-link></li>
-                      </ul>
-                    </li>
-                    <li><nuxt-link class="pagess" to="#">Pages</nuxt-link>
-                      <ul class="sub-menu">
-                        <li><nuxt-link to="/about">About</nuxt-link></li>
-                        <li><nuxt-link to="/team">team</nuxt-link></li>
-                        <li><nuxt-link to="/faq">FAQ</nuxt-link></li>
-                        <li><nuxt-link to="/review">Review</nuxt-link></li>
-                        <li><nuxt-link to="/pricing">Pricing</nuxt-link></li>
-                      </ul>
-                    </li>
-                    <li><nuxt-link class="pagess" to="#">Services</nuxt-link>
-                      <ul class="sub-menu">
-                        <li><nuxt-link to="/service">All services</nuxt-link></li>
-                        <li><nuxt-link to="/service-details">Service-details</nuxt-link></li>
-                      </ul>
-                    </li>
-                    <li><nuxt-link class="pagess" to="#">Works</nuxt-link>
-                      <ul class="sub-menu">
-                        <li><nuxt-link to="/project">Projects 01</nuxt-link></li>
-                        <li><nuxt-link to="/project-2">Projects 02</nuxt-link></li>
-                        <li><nuxt-link to="/project-details">Project details</nuxt-link></li>
-                      </ul>
-                    </li>
-                    <li><nuxt-link class="pagess" to="#">Blog</nuxt-link>
-                      <ul class="sub-menu">
-                        <li><nuxt-link to="/blog">Blog grid</nuxt-link></li>
-                        <li><nuxt-link to="/blog-sidebar">Blog Sidebar</nuxt-link></li>
-                        <li><nuxt-link to="/blog-details">Blog Details</nuxt-link></li>
-                      </ul>
-                    </li>
-                    <li><nuxt-link to="/contact">contacts</nuxt-link></li>
-                  </ul>
-                </div>
-              </div>
-            </nav>
-            <!-- mainmenu end -->
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- header-area end -->
-    <!-- mobile-menu-area start -->
-    <div class="mobile-menu-area hidden-lg hidden-md hidden-sm">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="mobile-menu">
-              <div class="logo">
-                <nuxt-link to="/"><img src="/img/logo/logo2.png" alt="" /></nuxt-link>
-              </div>
-              <nav id="dropdown">
-                <ul>
-                  <li><nuxt-link class="pagess" to="#">Home</nuxt-link>
-                    <ul class="sub-menu">
-                      <li><nuxt-link to="/">Home 01</nuxt-link></li>
-                      <li><nuxt-link to="/index-2">Home 02</nuxt-link></li>
-                      <li><nuxt-link to="/index-3">Home 03</nuxt-link></li>
-                    </ul>
-                  </li>
-                  <li><nuxt-link class="pagess" to="#">Pages</nuxt-link>
-                    <ul class="sub-menu">
-                      <li><nuxt-link to="/about">About</nuxt-link></li>
-                      <li><nuxt-link to="/team">team</nuxt-link></li>
-                      <li><nuxt-link to="/faq">FAQ</nuxt-link></li>
-                      <li><nuxt-link to="/review">Review</nuxt-link></li>
-                      <li><nuxt-link to="/pricing">Pricing</nuxt-link></li>
-                    </ul>
-                  </li>
-                  <li><nuxt-link class="pagess" to="#">Services</nuxt-link>
-                    <ul class="sub-menu">
-                      <li><nuxt-link to="/service">All services</nuxt-link></li>
-                      <li><nuxt-link to="/service-details">Service-details</nuxt-link></li>
-                    </ul>
-                  </li>
-                  <li><nuxt-link class="pagess" to="#">Works</nuxt-link>
-                    <ul class="sub-menu">
-                      <li><nuxt-link to="/project">Projects 01</nuxt-link></li>
-                      <li><nuxt-link to="/project-2">Projects 02</nuxt-link></li>
-                      <li><nuxt-link to="/project-details">Project details</nuxt-link></li>
-                    </ul>
-                  </li>
-                  <li><nuxt-link class="pagess" to="#">Blog</nuxt-link>
-                    <ul class="sub-menu">
-                      <li><nuxt-link to="/blog">Blog grid</nuxt-link></li>
-                      <li><nuxt-link to="/blog-sidebar">Blog Sidebar</nuxt-link></li>
-                      <li><nuxt-link to="/blog-details">Blog Details</nuxt-link></li>
-                    </ul>
-                  </li>
-                  <li><nuxt-link to="/contact">contacts</nuxt-link></li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- mobile-menu-area end -->
-  </header>
-  <!-- header end -->
+<template lang="pug">
+  header.header-one
+    // Start top bar
+    .topbar-area.fix.hidden-xs
+      .container
+        .row
+          .col-md-9.col-sm-9
+            .topbar-left
+              ul
+                li
+                  a(href='#')
+                    i.fa.fa-envelope
+                    |  info@MyWeb3.com
+                li
+                  a(href='#')
+                    i.fa.fa-phone-square
+                    |  +3234-7665545
+                li
+                  a(href='#')
+                    i.fa.fa-clock-o
+                    |  Mon - Fri: 10:00 - 18:00
+          .col-md-3.col-sm-3
+            .top-social
+              ul
+                li
+                  a(href='#')
+                    i.fa.fa-skype
+                li
+                  a(href='#')
+                    i.fa.fa-twitter
+                li
+                  a(href='#')
+                    i.fa.fa-facebook
+                li
+                  a(href='#')
+                    i.fa.fa-google
+    // End top bar
+    // header-area start
+    #sticker.header-area.hidden-xs
+      .container
+        .row
+          // logo start
+          .col-md-3.col-sm-3
+            .logo
+              // Brand
+              nuxt-link.navbar-brand(to='/')
+                img(src='/img/logo/logo2.png' alt='')
+            // logo end
+          .col-md-9.col-sm-9
+            .header-right-link
+              // search option end
+              nuxt-link.s-menu(to='login') Get in Estimate
+            // mainmenu start
+            nav.navbar.navbar-default
+              #navbar-example.collapse.navbar-collapse
+                .main-menu
+                  ul.nav.navbar-nav.navbar-right
+                    li
+                      nuxt-link.pagess(to='#') Home
+                      ul.sub-menu
+                        li
+                          nuxt-link(to='/') Home 01
+                        li
+                          nuxt-link(to='/index-2') Home 02
+                        li
+                          nuxt-link(to='/index-3') Home 03
+                    li
+                      nuxt-link.pagess(to='#') Pages
+                      ul.sub-menu
+                        li
+                          nuxt-link(to='/about') About
+                        li
+                          nuxt-link(to='/team') team
+                        li
+                          nuxt-link(to='/faq') FAQ
+                        li
+                          nuxt-link(to='/review') Review
+                        li
+                          nuxt-link(to='/pricing') Pricing
+                    li
+                      nuxt-link.pagess(to='#') Services
+                      ul.sub-menu
+                        li
+                          nuxt-link(to='/service') All services
+                        li
+                          nuxt-link(to='/service-details') Service-details
+                    li
+                      nuxt-link.pagess(to='#') Works
+                      ul.sub-menu
+                        li
+                          nuxt-link(to='/project') Projects 01
+                        li
+                          nuxt-link(to='/project-2') Projects 02
+                        li
+                          nuxt-link(to='/project-details') Project details
+                    li
+                      nuxt-link.pagess(to='#') Blog
+                      ul.sub-menu
+                        li
+                          nuxt-link(to='/blog') Blog grid
+                        li
+                          nuxt-link(to='/blog-sidebar') Blog Sidebar
+                        li
+                          nuxt-link(to='/blog-details') Blog Details
+                    li
+                      nuxt-link(to='/contact') contacts
+            // mainmenu end
+    // header-area end
+    // mobile-menu-area start
+    .mobile-menu-area.hidden-lg.hidden-md.hidden-sm
+      .container
+        .row
+          .col-md-12
+            .mobile-menu
+              .logo
+                nuxt-link(to='/')
+                  img(src='/img/logo/logo2.png' alt='')
+              nav#dropdown
+                ul
+                  li
+                    nuxt-link.pagess(to='#') Home
+                    ul.sub-menu
+                      li
+                        nuxt-link(to='/') Home 01
+                      li
+                        nuxt-link(to='/index-2') Home 02
+                      li
+                        nuxt-link(to='/index-3') Home 03
+                  li
+                    nuxt-link.pagess(to='#') Pages
+                    ul.sub-menu
+                      li
+                        nuxt-link(to='/about') About
+                      li
+                        nuxt-link(to='/team') team
+                      li
+                        nuxt-link(to='/faq') FAQ
+                      li
+                        nuxt-link(to='/review') Review
+                      li
+                        nuxt-link(to='/pricing') Pricing
+                  li
+                    nuxt-link.pagess(to='#') Services
+                    ul.sub-menu
+                      li
+                        nuxt-link(to='/service') All services
+                      li
+                        nuxt-link(to='/service-details') Service-details
+                  li
+                    nuxt-link.pagess(to='#') Works
+                    ul.sub-menu
+                      li
+                        nuxt-link(to='/project') Projects 01
+                      li
+                        nuxt-link(to='/project-2') Projects 02
+                      li
+                        nuxt-link(to='/project-details') Project details
+                  li
+                    nuxt-link.pagess(to='#') Blog
+                    ul.sub-menu
+                      li
+                        nuxt-link(to='/blog') Blog grid
+                      li
+                        nuxt-link(to='/blog-sidebar') Blog Sidebar
+                      li
+                        nuxt-link(to='/blog-details') Blog Details
+                  li
+                    nuxt-link(to='/contact') contacts
 </template>
 
 <script>
